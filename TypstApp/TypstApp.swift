20faf13ebcf6fcebe24433e06e8be01af1ce91f9
TianKai Ma <tiankaima@163.com>
2023-11-13 16:23:46 +0800

    chore: fix navigation bar issue

--- TypstApp/TypstApp.swift
+++ TypstApp/TypstApp.swift
@@ -6,13 +6,33 @@
 //
 
 import SwiftUI
+import UIKit
 
 @main
 struct TypstApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         DocumentGroup(newDocument: TypstFile()) { config in
             ContentView(document: config.$document)
-                .toolbarRole(.automatic)
+                .toolbarRole(.navigationStack)
         }
     }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = .systemBackground
+        appearance.titleTextAttributes = [.foregroundColor: UIColor.label]
+        appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.label]
+        
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
+        
+        return true
+    }
+    
 }
--- TypstApp/View/ContentView.swift
+++ TypstApp/View/ContentView.swift
@@ -55,7 +55,9 @@ struct ContentView: View {
             self.isRendering = true
         }
         
-        self.pdf = document.renderPDF()
+        Task.detached {
+            self.pdf = document.renderPDF()
+        }
         
         withAnimation(.smooth(duration: 3)) {
             self.isRendering = false
@@ -81,45 +83,47 @@ struct ContentView: View {
                     PDFView(Binding($pdf)!)
                 }
             }
-            .ignoresSafeArea(.keyboard)
-            .toolbar {
-                ToolbarItemGroup(placement: .primaryAction) {
-                    Button {
-                        render()
-                    } label: {
-                        if isRendering {
-                            Text("Rendering")
-                                .foregroundColor(.red)
-                        } else {
-                            Text("Build")
-                        }
-                    }
-                    .keyboardShortcut("r", modifiers: .command)
-                    
-                    Toggle(isOn: $autoBuild) {
-                        Text("Auto Build")
-                    }
-                    
-                    
-
-                    if let pdf {
-                        ShareLink(item: pdf.documentURL!) {
-                            Text("Share")
-                        }
+        }
+        .ignoresSafeArea(.keyboard)
+        .navigationBarBackButtonHidden()
+        .toolbar {
+            ToolbarItemGroup {
+                if isRendering {
+                    Text("Rendering")
+                        .foregroundColor(.red)
+                }
+                
+                Button {
+                    render()
+                } label: {
+                    Text("Build")
+                }
+                .disabled(isRendering)
+                .keyboardShortcut("r", modifiers: .command)
+                
+                Toggle(isOn: $autoBuild) {
+                    Text("Auto Build")
+                }
+                
+                
+                
+                if let pdf {
+                    ShareLink(item: pdf.documentURL!) {
+                        Text("Share")
                     }
                 }
             }
-            .onAppear {
-                Task.detached {
-                    var oldValue = ""
-                    repeat {
-                        if oldValue != document.code {
-                            render()
-                            oldValue = document.code
-                        }
-                        try await Task.sleep(nanoseconds: 2_000_000_000)  // refresh every 2 sec
-                    } while autoBuild
-                }
+        }
+        .onAppear {
+            Task.detached {
+                var oldValue = ""
+                repeat {
+                    if oldValue != document.code {
+                        render()
+                        oldValue = document.code
+                    }
+                    try await Task.sleep(nanoseconds: 2_000_000_000)  // refresh every 2 sec
+                } while autoBuild
             }
         }
     }
