7a90e703cd5aa0bb02dcc416c54bc4dfdbf504a5
TianKai Ma <tiankaima@163.com>
2023-11-13 16:41:39 +0800

    chore: clean definitions

--- TypstApp/Model/TypstFile.swift
+++ TypstApp/Model/TypstFile.swift
@@ -64,3 +64,30 @@ extension TypstFile: FileDocument {
         return .init(regularFileWithContents: data)
     }
 }
+
+struct TypstTitle {
+    var id = UUID()
+    var depth: Int
+    var content: String
+}
+
+extension [TypstTitle] {
+    static func parseFrom(_ code: String) -> [TypstTitle] {
+        // 1. split by \n
+        // 2. remove leading and trailing spaces
+        // 3. count "=" at the beginning -> depth
+        // 4. remove "=" at the beginning -> content
+        
+        code
+            .split(separator: "\n")
+            .compactMap { line in
+                let trimmed = line.trimmingCharacters(in: .whitespaces)
+                let depth = trimmed.prefix(while: { $0 == "=" }).count
+                let content = trimmed.drop(while: { $0 == "=" })
+                if depth == 0 {
+                    return nil
+                }
+                return TypstTitle(depth: depth, content: String(content))
+            }
+    }
+}
--- TypstApp/View/ContentView.swift
+++ TypstApp/View/ContentView.swift
@@ -8,34 +8,6 @@
 import PDFKit
 import SwiftUI
 
-struct TypstTitle {
-    var id = UUID()
-    var depth: Int
-    var content: String
-}
-
-extension [TypstTitle] {
-    static func parseFrom(_ code: String) -> [TypstTitle] {
-        // 1. split by \n
-        // 2. remove leading and trailing spaces
-        // 3. count "=" at the beginning -> depth
-        // 4. remove "=" at the beginning -> content
-        
-        code
-            .split(separator: "\n")
-            .compactMap { line in
-                let trimmed = line.trimmingCharacters(in: .whitespaces)
-                let depth = trimmed.prefix(while: { $0 == "=" }).count
-                let content = trimmed.drop(while: { $0 == "=" })
-                if depth == 0 {
-                    return nil
-                }
-                return TypstTitle(depth: depth, content: String(content))
-            }
-    }
-}
-
-
 struct ContentView: View {
     @Environment(\.colorScheme) private var colorScheme: ColorScheme
     @Binding var document: TypstFile
